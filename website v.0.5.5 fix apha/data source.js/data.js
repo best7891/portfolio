21a02b16b3dd14bc0abe29f9f6ee53892f06da06
best7891 <ertyuo12345gamil.com@gmail.com>
2026-08-04 01:08:08 +0700

    Add files via upload

---
+++ website v.0.5.5 fix apha/data source.js/data.js
@@ -0,0 +1,6 @@
+//*main js simple*//
+function sayHello() {
+ document.getElementById("sent").innerHTML = ('สวัสดีผมเป็นนักศึกษาปีที่ 4 กําลังฝึกทําเว็บไซต์ขนาน เล็ก โดยใช่ 3ภาษาเขียนขึ้นมา')
+}
+
+
